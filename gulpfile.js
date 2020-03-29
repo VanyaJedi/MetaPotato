80350790f14d02ff52da5495afebbc27f2cd15ae
Ivan <vanya71310@gmail.com>
2020-03-30 00:18:08 +0300

    add first views

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-
+var svgSprite = require("gulp-svg-sprite");
+var cheerio = require("gulp-cheerio");
+var svgmin = require('gulp-svgmin');
+var cleanSvg = require('gulp-cheerio-clean-svg');
+var svgo = require('gulp-svgo');
 
 gulp.task("sass", function () {
     return gulp.src('./wwwroot/scss/style.scss')
@@ -10,4 +14,22 @@ gulp.task("sass", function () {
         .pipe(gulp.dest("./wwwroot/css"));
 });
 
+gulp.task("svgSprite", function () {
+    return gulp.src("./wwwroot/img/icons/*.svg")
+        .pipe(svgmin())
+        .pipe(cheerio(cleanSvg()))
+        .pipe(replace("&gt;", ">"))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "sprite.svg"
+                }
+            }
+        }))
+        .pipe(gulp.dest("./wwwroot/img"));
+});
+
+
+
 gulp.watch("./wwwroot/scss/**/*.{scss,sass}", gulp.series("sass"));
+gulp.watch("./wwwroot/img/icons/*.svg", gulp.series("svgSprite"));
