2b4444b092b76d2f9b3e2beaf0106793f32816f8
Nikolay Nekipelov <Nek171>
2020-04-20 17:30:39 +0300

    Group in SignalR

--- ChatHub.cs
+++ ChatHub.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace MetaPotato
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string  message, string userName)
+        public async Task Send(string  message, string roomName)
         {
-            await this.Clients.All.SendAsync("Send", message, userName);
+           await this.Clients.Group(roomName).SendAsync("Send", message, roomName);
         }
+
+        public async Task JoinGroup(string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+           return base.OnConnectedAsync();
+        }
+
+     /*   public override Task OnDisconnectedAsync(bool stopCalled)
+        {
+            return base.OnDisconnectedAsync(stopCalled);
+        }
+        */
     }
 }
