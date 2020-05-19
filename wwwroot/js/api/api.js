1179733a3d0ae2006ebdf49790c57ca500fb1ea4
Ivan <vanya71310@gmail.com>
2020-05-19 20:26:22 +0300

    add components

---
+++ wwwroot/js/api/api.js
@@ -0,0 +1,37 @@
+﻿
+const URL_TEST_MESSAGES = 'Messages/Messages/'
+
+export default class Api {
+
+    _load({ url, method = `GET`, body = null, headers = new Headers() }, auth = null) {
+        if (auth) {
+            headers.append(`Authorization`, auth);
+        }
+        
+        return fetch(url, { method, body, headers })
+            .then(this._checkStatus)
+            .catch((err) => {
+                throw err;
+            });
+    }
+
+    _checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        } else {
+            throw new Error(`${response.status}: ${response.statusText}`);
+        }
+    }
+
+    _onErrorMessages() {
+        return [];
+    }
+
+    getMessages(chatRoomId) {
+        return this._load({ url: `${URL_TEST_MESSAGES}?chatRoomId=${chatRoomId}`})
+            .then((response) => response.json())
+            .catch(this._onError);
+    }
+
+
+}
---
+++ wwwroot/js/controllers/chatController.js
@@ -0,0 +1,98 @@
+﻿import hubConnection from "../modules/signalR.js";
+import { render } from "../utils/manipulateDOM.js";
+import Message from "../components/messageComponent.js";
+import Chat from "../components/chatComponent.js";
+import Api from "../api/api.js";
+
+const usersElement = document.querySelector('.users'); //Razor rendering
+const messagesContainer = document.querySelector('.messages__list');
+const sendBtn = document.querySelector('.messages__send-btn');
+const typeArea = document.querySelector('.messages__typeArea');
+const userList = document.querySelector('.users__list');
+
+const URL_TEST_MESSAGES = 'Messages/Messages/'
+
+export default class ChatController {
+
+    constructor(hub, api) {
+        this._hub = hub;
+        this._api = api;
+
+        this._currentChat = 1; //временный дефолтный чатрум, далее нужно брать при загрузки с сервера последний чат
+
+        this._chatComponent = new Chat();
+
+        this.getAndRenderMessagesHandler = this.getAndRenderMessagesHandler.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+
+        this.setHandlers(); // hang up all handlers at initialization 
+    }
+
+    setHubSendHandler(handler) {
+        this._hub.on('send', handler);
+    }
+
+    startHub() {
+        this._hub.start();
+    }
+
+
+    renderMessages(messages) {
+        messagesContainer.innerHTML = '';
+        messages.forEach((message) => {
+            const messageComponent = new Message(message.MessageText, message.UserName);
+            render(messagesContainer, messageComponent);
+        });
+    }
+
+    getAndRenderMessagesHandler(evt) {
+        const targetUserElement = evt.target.closest('.users__item');
+        if (targetUserElement) {
+            const chatRoomId = targetUserElement.dataset.chatroom;
+            const isSameChat = (chatRoomId === this._currentChat);
+            if (!isSameChat) {
+                messagesContainer.innerHTML = '';
+                this._hub.invoke('JoinGroup', chatRoomId);
+                this._api.getMessages(chatRoomId).
+                    then((messages) => {
+                        this._currentChat = chatRoomId;
+                        this.renderMessages(messages);
+                    })
+            }
+        }
+    }
+
+    setChatRoomSelectHandler() {
+        this._chatComponent.setUserItemClickHandler(this.getAndRenderMessagesHandler);
+    }
+
+    subscribeChatMediaEvents() {
+        this._chatComponent.subscribeMediaEvents();
+    }
+
+
+    sendMessage() {
+        const textMessage = this._chatComponent.typeArea.innerText;
+        if (!textMessage) {
+            return;
+        }
+        this._hub.invoke('Send', textMessage, this._currentChat).
+            then(() => {
+                typeArea.innerText = '';
+                const messageComponent = new Message(textMessage, "Это я");
+                render(this._chatComponent.messagesList, messageComponent);
+            });
+    }
+
+    setSendMessageHandler() {
+        this._chatComponent.setSendMessageHandler(this.sendMessage);
+    }
+
+    setHandlers() {
+        this._chatComponent.setScreenHandlers();
+        this.setChatRoomSelectHandler();
+        this.setSendMessageHandler();
+    }
+
+
+}
