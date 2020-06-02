39d64e947dab5e547ebe6cdb39b2228d27b8e801
Ivan <vanya71310@gmail.com>
2020-06-02 15:11:14 +0300

    modified_auth_WIP

--- ViewModels/LoginModel.cs
+++ ViewModels/LoginModel.cs
@@ -11,8 +11,15 @@ namespace MetaPotato.ViewModels
         [Required(ErrorMessage = "Не указан Login")]
         public string Login { get; set; }
 
+        [Required(ErrorMessage = "Не указан Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
     }
 }
--- ViewModels/RegisterModel.cs
+++ ViewModels/RegisterModel.cs
@@ -11,6 +11,10 @@ namespace MetaPotato.ViewModels
         [Required(ErrorMessage = "Не указан Login")]
         public string Login { get; set; }
 
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
